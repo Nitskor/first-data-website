7a924ccc67a63275a7cbc4750238c6f850a9ac14
Nitskor <163732966+Nitskor@users.noreply.github.com>
2024-09-09 12:56:33 +0000

    Application database created

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template,request
 from sqlalchemy import text
-from database import load_jobs
+from database import load_jobs,load_job_from_db,add_application_to_db
 app = Flask(__name__)
 
 JOBS = [{'id':1,
@@ -29,6 +29,19 @@ JOBS = [{'id':1,
 def hello_world():
     jobs = load_jobs()
     return render_template('home.html',jobs = jobs)
+
+@app.route('/jobs/<id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found",404
+    return render_template('jobspage.html',job = job)
+
+@app.route('/jobs/<id>/apply',methods=['post'])
+def apply_to_jobs(id):
+    data = request.form
+    add_application_to_db(id,data)
+    return render_template('application_submited.html',application=data)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,8 @@
+from flask import jsonify
 import sqlalchemy
 from sqlalchemy import create_engine,text
 import os
+import json
 
 
 db_URL = os.environ['DB_CONNECTION_STR']
@@ -13,4 +15,24 @@ def load_jobs():
       result = conn.execute(text("select * from jobs"))
       jobs = result.mappings().all()
   return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {'val':id})
+    job =[dict(row) for row in result.mappings().all()] 
+    return job
+
+def add_application_to_db(job_id,data):
+  
+  with engine.connect() as conn:
+    with conn.begin():
+      query = text("INSERT INTO applications (job_id,full_name,email,age,work_experience) VALUES(:job_id,:full_name,:email,:age,:work_experience)")
+      params = {'job_id':job_id,'full_name':data['full_name'],'email':data['email'],'age':data['age'],'work_experience':data['work_experience']}
+      conn.execute(query,params)
+
+
+
+  
+    
+
 
